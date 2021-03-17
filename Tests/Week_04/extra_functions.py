15f3c90f7e081456d976eb67925b53c46901c738
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-17 19:07:18 +1000

    practice programing

---
+++ Tests/Week_04/extra_functions.py
@@ -0,0 +1,20 @@
+
+def main():
+    """Demo function scope."""
+    x = [3] # ints are immutable
+    print("in main, id is {}".format(id(x)))
+    z = function(x)
+    print("in main, id is {}".format(id(x)))
+
+
+def function(y: int) -> None:
+    print("in func, id is {}".format(id(y)))
+    y += [1]
+    print("in func, id is {}".format(id(y)))
+    return y
+
+
+#main()
+
+#print(main.__doc__)
+#print(function.__str__())
